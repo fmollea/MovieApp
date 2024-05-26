47bebc75687d65bd7b9513c40faeb55c9bf60e6a
fmollea <fmollea@gmail.com>
2024-05-26 13:47:29 -0300

    delete proguard files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
     compileOptions {
