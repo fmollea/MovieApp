ea4dd36bf862c8cd8fd5356dd79b04dca5abc9bd
fmollea <fmollea@gmail.com>
2024-05-27 18:03:25 -0300

    delete dont use imports

--- features/movielist/src/main/java/com/fmollea/movielist/ui/components/MovieList.kt
+++ features/movielist/src/main/java/com/fmollea/movielist/ui/components/MovieList.kt
@@ -5,19 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -26,7 +21,6 @@ import androidx.paging.compose.LazyPagingItems
 import coil.compose.rememberAsyncImagePainter
 import com.fmollea.domain.model.Movie
 import com.fmollea.domain.model.MovieDetail
-import okhttp3.internal.wait
 
 @Composable
 fun MovieList(
